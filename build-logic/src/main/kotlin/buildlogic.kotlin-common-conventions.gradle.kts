8abbf5d5fac07aa40e6f8c302baa9e5867c01656
Felipe Sena <felipedepaulasena@hotmail.com>
2026-09-19 12:04:54 -0300

    chore: remove linked list class

--- build-logic/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
@@ -24,6 +24,6 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(26)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
